2f89627abae3a1a99185b3cbd19015f8d92c60f5
ggoral <goral.gonzalo@gmail.com>
2016-09-05 19:02:17 -0300

    remove useless require

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'sinatra/reloader'
 
 require File.expand_path(File.join('helpers', 'application_helper'))
 require File.expand_path(File.join('controllers', 'application_controller'))
-require File.join(File.dirname(__FILE__), '../controllers', 'example_controller')
 
 Dir[File.join('helpers', '**/*._helper.rb')].each { |file| require File.expand_path(file) }
 Dir[File.join('controllers', '**/*_controller.rb')].each { |file| require File.expand_path(file) }
